5567a9ac3bcd56a55e6fca07cff95aa6bfa3aa33
kobori <shaun.white.haku@gmail.com>
2019-07-26 18:02:16 +0900

    jsファイルの修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,6 +4,7 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:followed_id])
     current_user.follow(@user)
+    #ユーザの要求に応じてフォーマとを変更
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
@@ -13,6 +14,7 @@ class RelationshipsController < ApplicationController
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow(@user)
+    #ユーザの要求に応じてフォーマとを変更
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
